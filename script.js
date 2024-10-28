8b6f5dd0e2a8c9266db44d0389de30fcfbbf5b8d
matteo <matteokalcich@gmail.com>
2024-10-28 21:47:05 +0100

    finished backend wordle logic

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
-var parola = 'SEDIA';
+let parola = 'SEDIA';
+let currentRow = 0;
 
 
-function sendWord(){
+function sendWord() {
+    let insertedWord = document.getElementById('wordToSend');
 
-    var t = document.getElementById('wordToSend');
+    if (insertedWord.value.length != 5) {
+        
+        alert('ERRORE: inserire una parola con 5 lettere');
 
-    if(t.value.length != 5){
+        insertedWord.value = '';
 
-        alert('ERRORE parola troppo corta');
+        return;
     }
 
-    if(t.value.toUpperCase() == parola.toUpperCase()){
+    showWord(insertedWord.value.toUpperCase());
+
+    if (insertedWord.value.toUpperCase() == parola) {
 
         alert('HAI VINTO!');
-        t.innerHTML = "";
         
-    } else{
+        insertedWord.value = '';
 
+        document.getElementById('wordToSend').disabled = true;
+
+        return;
+    }
+
+    currentRow++;
+
+    if (currentRow >= 5) {
+
+        alert('Hai perso! La parola era: ' + parola);
+
+        insertedWord.value = '';
+    }
+}
+
+function showWord(insertedWord) {
+
+    for (let i = 0; i < insertedWord.length; i++) {
+
+        const button = document.getElementById('R' + currentRow + 'C' + i);
+
+        button.innerHTML = '<p>' + insertedWord[i] + '</p';
         
+        button.disabled = false;
+
+        if (insertedWord[i] == parola[i]) {
+
+            button.style.backgroundColor = 'green';
+
+        } else if (parola.includes(insertedWord[i])) {
+
+            button.style.backgroundColor = 'yellow';
+
+        } else {
+
+            button.style.backgroundColor = 'gray';
+
+        }
     }
 }
